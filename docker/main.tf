3310d22ab6fe715a4bff5cd250942ea12a1b0c2e
Anirvan Chakraborty <anirvanc@cakesolutions.net>
2017-05-16 13:22:57 +0100

    Fixed custon Jenkins docker image building and pushing to ECR

--- docker/main.tf
+++ docker/main.tf
@@ -1,11 +1,19 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
 resource "aws_ecr_repository" "jenkins" {
-  name = "${var.image_name}"
+  name = "${var.jenkins_image_name}"
   provisioner "local-exec" {
     command = "./deploy-image.sh ${self.repository_url} ${var.jenkins_image_name}"
   }
 }
 
 variable "jenkins_image_name" {
-  default = "mycompany/jenkins"
+  default = "cakesolutions/jenkins"
   description = "Jenkins image name."
+}
+
+variable "region" {
+  default = "eu-west-2"
 }
